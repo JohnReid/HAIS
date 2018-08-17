6e24f402e1b4daed3aa952eeda954ff4e875d664
JohnReid <johnbaronreid@netscape.net>
2018-08-17 17:57:16 +0100

    Update HMC to use partial momentum refresh

--- hais/hmc.py
+++ hais/hmc.py
@@ -51,25 +51,18 @@ def leapfrog(x0, v0, eps, energy_fn):
   Simulate the Hamiltonian dynamics using leapfrog method. That is follow the 2nd step in the 5 step
   procedure in Section 2.3 of Sohl-Dickstein and Culpepper's paper.
   """
-  eps = .2
   epshalf = eps / 2.
   xhalf = x0 + epshalf * v0
   dE_dx = tf.gradients(tf.reduce_sum(energy_fn(xhalf)), xhalf)[0]
   v1 = v0 - eps * dE_dx
   x1 = xhalf + epshalf * v1
-  return x1, v1
+  return x1, -v1
 
 
-def hmc_move(x0, energy_fn, event_axes, eps):
+def hmc_move(x0, v0, energy_fn, event_axes, eps):
   """
   Make a HMC move.
   """
-  #
-  # Choose an initial velocity randomly.
-  # This does not use Sohl-Dickstein and Culpepper's idea of conserving
-  # momentum across iterations.
-  # TODO: fix this!
-  v0 = tf.random_normal(tf.shape(x0))
   #
   # STEP 2:
   # Simulate the dynamics of the system using leapfrog
@@ -85,7 +78,29 @@ def hmc_move(x0, energy_fn, event_axes, eps):
   E0 = hamiltonian(x0, v0, energy_fn, event_axes)
   E1 = hamiltonian(x1, v1, energy_fn, event_axes)
   accept = metropolis_hastings_accept(E0=E0, E1=E1)
-  return accept, x1, v1
+  # print('accept: {}'.format(accept.shape))
+  # print('x0: {}'.format(x0.shape))
+  # print('x1: {}'.format(x1.shape))
+  # Expand the accept (which has batch shape) to full (batch + event) shape.
+  accept_tiled = tf_expand_tile(accept, x1)
+  xdash = tf.where(accept_tiled, x1, x0)
+  vdash = tf.where(accept_tiled, -v1, v0)
+  # print('xdash: {}'.format(xdash.shape))
+  #
+  # STEP 4:
+  # Partial momentum refresh.
+  # See Eqn 11. of Culpepper et al. 2011 "Building a better probabilistic model of images by factorization"
+  gamma = 1 - tf.exp(eps * tf.log(1/2.))
+  # There is some disagreement between the above paper and the description of STEP 4.
+  # Specifically the second sqrt below is omitted in the description of STEP 4.
+  # Note also that we removed the leading minus '-' from the vtilde equation
+  # as this improved performance
+  r = tf.random_normal(tf.shape(vdash))
+  vtilde = tf.sqrt(1 - gamma) * vdash + tf.sqrt(gamma) * r
+  #
+  # STEP 5:
+  # Return state
+  return accept, xdash, vtilde
 
 
 def hmc_updates(x0, eps, smoothed_acceptance_rate, x1, accept,
@@ -98,14 +113,6 @@ def hmc_updates(x0, eps, smoothed_acceptance_rate, x1, accept,
     - update the step size adaptively according to whether the acceptance rate is high or low
     - smooth the acceptance rates (over the iterations)
   """
-  #
-  # Choose the new position according to whether we accepted or rejected
-  # First expand the accept (which has batch shape) to full (batch + event) shape.
-  print('accept: {}'.format(accept.shape))
-  print('x0: {}'.format(x0.shape))
-  print('x1: {}'.format(x1.shape))
-  new_x = tf.where(tf_expand_tile(accept, x1), x1, x0)
-  print('new_x: {}'.format(new_x.shape))
   #
   # Increase or decrease step size according to whether we are above or below target (average) acceptance rate
   # print('smoothed_acceptance_rate: {}'.format(smoothed_acceptance_rate.shape))
@@ -120,6 +127,13 @@ def hmc_updates(x0, eps, smoothed_acceptance_rate, x1, accept,
   new_eps = tf.clip_by_value(new_eps, clip_value_min=stepsize_min, clip_value_max=stepsize_max)
   #
   # Smooth the acceptance rate
-  new_acceptance_rate = tf.add(acceptance_decay * smoothed_acceptance_rate,
-                               (1.0 - acceptance_decay) * tf.to_float(accept))
-  return new_x, new_eps, new_acceptance_rate
+  new_acceptance_rate = smooth_acceptance_rate(accept, smoothed_acceptance_rate, acceptance_decay)
+  return xdash, new_eps, new_acceptance_rate
+
+
+def smooth_acceptance_rate(accept, old_acceptance_rate, acceptance_decay):
+  #
+  # Smooth the acceptance rate
+  assert accept.shape == old_acceptance_rate.shape
+  new_acceptance_rate = tf.add(acceptance_decay * old_acceptance_rate, (1.0 - acceptance_decay) * tf.to_float(accept))
+  return new_acceptance_rate
